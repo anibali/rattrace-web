28f00da41fd81f19a4922a995b5c454462e92dfe
Aiden Nibali <dismaldenizen@gmail.com>
2015-06-16 10:45:57 +1000

    Don't nest params

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,5 @@
 class ReportsController < ApplicationController
+  wrap_parameters false # Disable automatic creation of params["report"]
   skip_before_filter :verify_authenticity_token
   http_basic_authenticate_with name: "udp_server", password: "super_secret_password", only: :create
 
